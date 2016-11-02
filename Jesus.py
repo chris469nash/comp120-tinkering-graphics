fd139b1719a3c4b4ed96eb4f6b0f8d4109c8019d
hannahlu <hannah.louise.utley@gmail.com>
2016-11-02 10:37:43 +0000

    Bantz

--- Jesus.py
+++ Jesus.py
@@ -16,25 +16,17 @@ image = pygame.image.load('Jesus.jpg')
 a = 100
 b = 200
 
-sprite = pygame.sprite.Sprite()
-sprite.image = image
-sprite.rect = image.get_rect()
 
 font = pygame.font.SysFont('Sans', 38)
 text = font.render("When Your Phone Runs Out", True, (255, 255, 255))
-#screen.blit (text,[,])
+text2 = font.render("In a Lecture", True, (255, 255, 255))
+screen.blit(image, (0, 0))
+screen.blit(text,(0, 0))
+screen.blit(text2,(115, 500))
 
-sprite.image.blit(text, sprite.rect)
-
-group = pygame.sprite.Group()
-group.add(sprite)
-group.draw(screen)
 
 pygame.display.flip()
 
-print 'bits per pixel:'
-print 'image', image.get_bitsize()
-print 'screen', screen.get_bitsize()
 
 while True:
     for event in pygame.event.get():
